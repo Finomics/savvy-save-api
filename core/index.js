4007de266b78f230a3ce6058692a4109bd0df276
MuhammadHammadAhmed <42751169+MuhammadHammadAhmed@users.noreply.github.com>
2024-10-21 10:41:41 +0500

    first commit

---
+++ core/index.js
@@ -0,0 +1,8 @@
+
+const PORT = process.env.PORT || 5000;
+const dbURI = process.env.dbURI || "mongodb+srv://savvysave:Pakistan1947!@savvyfinance.n9kb2.mongodb.net/?retryWrites=true&w=majority&appName=SavvyFinance";
+
+module.exports = {
+    PORT: PORT,
+    dbURI: dbURI,
+}
